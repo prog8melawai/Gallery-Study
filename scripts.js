1e71944f7333dea87c89f34be8447416aefaa565
prog8melawai <prog8melawai@gmail.com>
2024-07-10 13:38:19 +0700

    parallax add

--- scripts.js
+++ scripts.js
@@ -27,3 +27,36 @@ function displaySlides(n) {
 
 }
 
+
+//auto invoke function
+(function() { 
+    document.addEventListener("mousemove", parallax);
+    //elemen apa saja yang dikasih parallax
+    const elem1 = document.querySelector(".img1")
+    const elem2 = document.querySelector(".img2")
+    const elem3 = document.querySelector(".img3")
+    const elem4 = document.querySelector(".img4")
+
+    //e = event --> lihat event listener diatas
+    function parallax(e) {
+        let w = window.innerWidth / 2; 
+        let h = window.innerHeight / 2;
+        //posisi mouse
+        let mouseX = e.clientX;
+        let mouseY = e.clientY;
+
+        //logic parallax
+        let depth1 = `${50-(mouseX-w)*0.01}% ${50-(mouseY-h)*0.01}%`;
+        let depth2 = `${50-(mouseX-w)*0.02}% ${50-(mouseY-h)*0.02}%`;
+        let depth3 = `${50-(mouseX-w)*0.06}% ${50-(mouseY-h)*0.06}%`;
+        let x = `${depth3}, ${depth2}, ${depth1}`;
+        console.log(x);
+
+        //implement ke element
+        elem1.style.backgroundPosition = x;
+        elem2.style.backgroundPosition = x;
+        elem3.style.backgroundPosition = x;
+        elem4.style.backgroundPosition = x;
+
+    }   
+})();
